144e9d8c9d81cf403b9b872d03d878d80af655d5
Nicolas Assouad <nicolasassouad75@gmail.com>
2025-07-17 10:29:31 +0200

    Deploy contract to dev environment

--- clis/src/config/CONFIGURATIONS.ts
+++ clis/src/config/CONFIGURATIONS.ts
@@ -11,6 +11,9 @@ export type Configuration = {
 export const CONFIGURATIONS: Record<Environment, Configuration> = {
   dev: {
     tokens: [
+      { name: 'Dev EUTBL', symbol: 'EUTBL', decimals: 5 },
+      { name: 'Dev USTBL', symbol: 'USTBL', decimals: 5 },
+      { name: 'Dev eurUSTBL', symbol: 'eurUSTBL', decimals: 5 },
       { name: 'Dev UKTBL', symbol: 'UKTBL', decimals: 5 },
       { name: 'Dev SPKCC', symbol: 'SPKCC', decimals: 5 },
       { name: 'Dev eurSPKCC', symbol: 'eurSPKCC', decimals: 5 },
@@ -18,6 +21,9 @@ export const CONFIGURATIONS: Record<Environment, Configuration> = {
   },
   staging: {
     tokens: [
+      { name: 'Staging EUTBL', symbol: 'EUTBL', decimals: 5 },
+      { name: 'Staging USTBL', symbol: 'USTBL', decimals: 5 },
+      { name: 'Staging eurUSTBL', symbol: 'eurUSTBL', decimals: 5 },
       { name: 'Staging UKTBL', symbol: 'UKTBL', decimals: 5 },
       { name: 'Staging SPKCC', symbol: 'SPKCC', decimals: 5 },
       { name: 'Staging eurSPKCC', symbol: 'eurSPKCC', decimals: 5 },
@@ -25,6 +31,9 @@ export const CONFIGURATIONS: Record<Environment, Configuration> = {
   },
   preprod: {
     tokens: [
+      { name: 'Preprod EUTBL', symbol: 'EUTBL', decimals: 5 },
+      { name: 'Preprod USTBL', symbol: 'USTBL', decimals: 5 },
+      { name: 'Preprod eurUSTBL', symbol: 'eurUSTBL', decimals: 5 },
       { name: 'Preprod UKTBL', symbol: 'UKTBL', decimals: 5 },
       { name: 'Preprod SPKCC', symbol: 'SPKCC', decimals: 5 },
       { name: 'Preprod eurSPKCC', symbol: 'eurSPKCC', decimals: 5 },
